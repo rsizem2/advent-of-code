4733092bea26bb2f286b36cb8af8459507bbedc9
rsizem2 <sizemore.robert.k@gmail.com>
2020-12-02 13:34:07 -0500

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,30 @@
+
+
+def read_file():
+    with open('day1.txt') as file:
+        temp = [int(line.strip()) for line in file]
+    return temp
+            
+def puzzle1():
+    numbers = read_file()
+    for i, x in enumerate(numbers):
+        for y in numbers[i:]:
+            if x + y == 2020:
+                print('Solution 1:', x*y)
+                return
+    print('No Solution Found!')
+
+def puzzle2():
+    numbers = read_file()
+    for i, x in enumerate(numbers):
+        for j, y in enumerate(numbers[i:]):
+            if i + j >= 2020: 
+                continue
+            for z in numbers[j:]:
+                if x + y + z == 2020:
+                    print('Solution 2:', x*y*z)
+                    return
+    print('No Solution Found!')
+
+puzzle1()
+puzzle2()
