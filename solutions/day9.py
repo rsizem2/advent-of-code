8660d8c3495957f189f3303aa84372c68c9d1fd5
rsizem2 <sizemore.robert.k@gmail.com>
2020-12-09 01:25:32 -0500

    update day 9

--- solutions/day9.py
+++ solutions/day9.py
@@ -1,15 +1,81 @@
+from itertools import combinations
 
-def read_file():
-    with open("../input/day9_input.txt") as file:
-        pass
-    return list()
+def read_file(path = "../input/day9_input.txt"):
+    with open(path) as file:
+        temp = [int(line.strip()) for line in file]
+    return temp
 
+
+def test1():
+    numbers = read_file("../input/day9_test.txt")
+    preamble = 5
+    for i, number in enumerate(numbers[preamble:], start = preamble):
+        print("Checking", number)
+        found = False
+        for j, k in combinations(range(i-preamble,i),2):            
+            if numbers[j] + numbers[k] == number: 
+                found = True
+                break
+        if not found:
+            print(number)
+            return True
+    return False         
+    
+    
 def puzzle1():
-    pass
+    numbers = read_file()
+    preamble = 25
+    for i, number in enumerate(numbers[preamble:], start = preamble):
+        print("Checking", number)
+        found = False
+        for j, k in combinations(range(i-preamble,i),2):            
+            if numbers[j] + numbers[k] == number: 
+                found = True
+                break
+        if not found:
+            print(number)
+            return True
+    return False         
+    
+def test2():
+    numbers = read_file("../input/day9_test.txt")
+    start = 0
+    end = 1
+    ans = 127
+    temp = sum(numbers[start:end])
+    try: 
+        while(temp != ans):
+            if temp > ans:
+                start += 1
+            else:
+                end += 1
+            temp = sum(numbers[start:end])
+    except:
+        print("No such range found.")
+    small = min(numbers[start:end])
+    large = max(numbers[start:end])
+    print(small + large)
+
 
 def puzzle2():
-    pass
+    numbers = read_file()
+    start = 0
+    end = 1
+    ans = 1398413738
+    temp = sum(numbers[start:end])
+    try: 
+        while(temp != ans):
+            if temp > ans:
+                start += 1
+            else:
+                end += 1
+            temp = sum(numbers[start:end])
+    except:
+        print("No such range found.")
+    small = min(numbers[start:end])
+    large = max(numbers[start:end])
+    print(small + large)
 
-puzzle1()
 puzzle2()
+
     
