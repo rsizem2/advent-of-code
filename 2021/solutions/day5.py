93d621dd9474f14200168a1e5858809ed2a4bbe6
Robert Sizemore <50637844+rsizem2@users.noreply.github.com>
2021-12-05 01:31:05 -0500

    day 5

--- 2021/solutions/day5.py
+++ 2021/solutions/day5.py
@@ -1,19 +1,78 @@
+import numpy as np
+from collections import Counter
+
 def read_file(test = True):
     if test:
-        filename = '../input/day5.txt'
+        filename = '../tests/day5.txt'
     else:
         filename = '../input/day5.txt'
     
     with open(filename) as file:
-        pass
+        temp = [line.strip().split(' -> ') for line in file]
+    points = list()
+    for x,y in temp:
+        points.append([tuple(map(int, x.split(','))), tuple(map(int, y.split(',')))])
     
-    return
+    return points
 
-def puzzle1():
-    pass
+def puzzle1(test = True):
+    points = read_file(test)
+    lines = list()
+    count = Counter()
+    for a,b in points:
+        x1,y1 = a
+        x2,y2 = b
+        if x1 == x2:
+            y1, y2 = sorted([y1,y2])
+            lines.append(set([(x1,y) for y in np.arange(y1,y2+1)]))
+        elif y1 == y2:
+            x1, x2 = sorted([x1,x2])
+            lines.append(set([(x,y1) for x in np.arange(x1,x2+1)]))
+    for points in lines:
+        count.update(points)
+    intersections = 0
+    for point, num in count.items():
+        if num > 1:
+            intersections += 1
+    print(intersections)
 
-def puzzle2():
-    pass
+def puzzle2(test = True):
+    points = read_file(test)
+    lines = list()
+    count = Counter()
+    for a,b in points:
+        x1,y1 = a
+        x2,y2 = b
+        if x1 == x2:
+            y1, y2 = sorted([y1,y2])
+            lines.append(set([(x1,y) for y in np.arange(y1,y2+1)]))
+        elif y1 == y2:
+            x1, x2 = sorted([x1,x2])
+            lines.append(set([(x,y1) for x in np.arange(x1,x2+1)]))
+        elif x1 < x2 and y1 < y2:
+            x = np.arange(x1,x2+1)
+            y = np.arange(y1,y2+1)
+            lines.append(set(zip(x,y)))
+        elif x1 < x2 and y1 > y2:
+            x = np.arange(x1,x2+1)
+            y = np.arange(y1,y2-1,-1)
+            lines.append(set(zip(x,y)))
+        elif x1 > x2 and y1 < y2:
+            x = np.arange(x1,x2-1,-1)
+            y = np.arange(y1,y2+1)
+            lines.append(set(zip(x,y)))
+        elif x1 > x2 and y1 > y2:
+            x = np.arange(x1,x2-1,-1)
+            y = np.arange(y1,y2-1,-1)
+            lines.append(set(zip(x,y)))
+            
+    for points in lines:
+        count.update(points)
+    intersections = 0
+    for point, num in count.items():
+        if num > 1:
+            intersections += 1
+    print(intersections)
 
-puzzle1()
-puzzle2()
+puzzle1(False)
+puzzle2(False)
