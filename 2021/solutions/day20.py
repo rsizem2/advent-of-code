ab7b7c2a6d8b10c6f98741e4dbdbdab449d00001
Robert Sizemore <50637844+rsizem2@users.noreply.github.com>
2021-12-22 01:16:08 -0500

    day 20

--- 2021/solutions/day20.py
+++ 2021/solutions/day20.py
@@ -1,4 +1,7 @@
+# Using convolutions in numpy
+
 import numpy as np
+from scipy.ndimage import convolve
 from itertools import combinations
 
 def read_file(test = True):
@@ -10,24 +13,46 @@ def read_file(test = True):
         image = list()
         for i, line in enumerate(file):
             if i == 0:
-                algorithm = line.strip()
+                algorithm = np.array([int(char == "#") for char in line.strip()]) 
             elif line.strip():
                 image.append(line.strip())
+    image = np.pad([[int(char == "#") for char in row] for row in image], (51,51))
     return algorithm, image
             
-
+        
+    
+def print_set(input_set):
+    pass
 
 def puzzle1(test = True):
     alg, img = read_file(test)
-    for line in img:
-        print(line)
-                
-                
+    
+    # kernel for convolution
+    kernel = 2**np.arange(9).reshape(3,3)
+    
+    for i in range(2):
+        
+        # compute number for each pixel
+        temp = convolve(img, kernel)
+        
+        # apply algorithm to light/dim each pixel
+        img = alg[temp]
+    print(img.sum())
+                    
 
 def puzzle2(test = True):
     alg, img = read_file(test)
-    for line in img:
-        print(line)
+    
+    # kernel for convolution
+    kernel = 2**np.arange(9).reshape(3,3)
+    
+    for i in range(50):
+        # compute number for each pixel
+        temp = convolve(img, kernel)
+        
+        # apply algorithm to light/dim each pixel
+        img = alg[temp]
+    print(img.sum())
 
 
 puzzle1()
