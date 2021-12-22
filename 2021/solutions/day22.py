1aefcd76547af22418688ebbc87aa31cbb6a4cb6
Robert Sizemore <50637844+rsizem2@users.noreply.github.com>
2021-12-22 14:15:43 -0500

    day 22
    finally caught up after skipping a few days

--- 2021/solutions/day22.py
+++ 2021/solutions/day22.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 def read_file(test = True):
     if test:
@@ -15,57 +16,72 @@ def read_file(test = True):
             temp.append((state, (x,y,z)))
     return temp
 
-def discretize(cube1, cube2):
-    assert overlap(cube1, cube2)
-    pass
-
-def area(cube):
-    x,y,z = cube
-    return (x[1]-x[0])*(y[1]-y[0])*(z[1]-z[0])
-
-def overlap(cube1, cube2):
-    x1,y1,z1 = cube1 
-    x2,y2,z2 = cube2 
-    if x1[0] > x2[1] or x1[1] < x2[0]: 
-        return False
-    elif y1[0] > y2[1] or y1[1] < y2[0]:
-        return False
-    elif z1[0] > z2[1] or z1[1] < z2[0]:
-        return False
-    return True
-    
-def truncate(cube):
-    x,y,z = cube
-    
-    if not overlap(cube, ((-50,50),(-50,50),(-50,50))):
-        return None
-    
-    # truncate
-    x_min, x_max = max(x[0], -50), min(x[1], 50)
-    y_min, y_max = max(y[0], -50), min(y[1], 50)
-    z_min, z_max = max(z[0], -50), min(z[1], 50)
-    return (x_min, x_max), (y_min, y_max), (z_min, z_max)
-
-def subtract(cube1, cube2):
-    pass
-
-    
-
 def puzzle1(test = True):
+    # calculate all unit cubes explicitly
     temp = read_file(test)
-    total = 0
-    for i, (state, coords) in enumerate(temp):
-        print(coords)
-        print(truncate(coords))
-        temp[i] = (state, truncate(coords))
-        total += area(coords)
-    print(total)
-                    
+    cubes = set()
+    for state, coords in temp:
+        x,y,z = coords
+        x1, x2 = x
+        y1, y2 = y
+        z1, z2 = z
+        for x in range(max(-50,x1), min(51,x2+1)):
+            for y in range(max(-50,y1), min(51,y2+1)):
+                for z in range(max(-50,z1), min(51,z2+1)):
+                    if state == 'on':
+                        cubes.add((x,y,z))
+                    elif state == 'off':
+                        cubes.discard((x,y,z))
+                    else:
+                        assert False
+    print(len(cubes))
+
+
+# helper for part2
+def intersect_pair(pair1, pair2):
+    assert pair1[0] <= pair1[1] and pair2[0] <= pair2[1]
+    if pair2[0] > pair1[1] or pair1[0] > pair2[1]:
+        return None
+    nums = sorted([pair1[0], pair1[1], pair2[0], pair2[1]])
+    return [nums[1], nums[2]]
+
+# helper for part2
+def intersect_cubes(cube1, cube2):
+    x1,y1,z1 = cube1
+    x2,y2,z2 = cube2
+    x = intersect_pair(x1, x2)
+    y = intersect_pair(y1, y2)
+    z = intersect_pair(z1, z2)
+    if not all([x, y, z]):
+        return None
+    return x, y, z
+
+# data structure for cube and it's empty spaces contained within
+class Cuboid:
+    def __init__(self, coords) -> None:
+        self.coords = coords
+        self.vacuums = []
+    def remove(self, coords):
+        shaved_coords = intersect_cubes(self.coords, coords)
+        if not shaved_coords:
+            return
+        for vacuum in self.vacuums:
+            vacuum.remove(shaved_coords)
+        self.vacuums.append(Cuboid(shaved_coords))
+    def volume(self):
+        x,y,z = self.coords
+        return (x[1]-x[0]+1)*(y[1]-y[0]+1)*(z[1]-z[0]+1)-sum(vacuum.volume() for vacuum in self.vacuums)
 
 def puzzle2(test = True):
     temp = read_file(test)
-        
+    cuboids = []
+    for state, coords in temp:
+        for cuboid in cuboids:
+            cuboid.remove(coords)
+        if state == 'on':
+            cuboids.append(Cuboid(coords))
 
+    print(sum(cuboid.volume() for cuboid in cuboids))
 
 puzzle1()
-puzzle2()
+puzzle2(False)
