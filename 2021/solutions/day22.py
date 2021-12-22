bc1c1c55f06e9f8abd22dede27a6ca2fdfc21cba
Robert Sizemore <50637844+rsizem2@users.noreply.github.com>
2021-12-22 04:38:59 -0500

    Update day22.py

--- 2021/solutions/day22.py
+++ 2021/solutions/day22.py
@@ -19,14 +19,100 @@ def area(cube):
     x,y,z = cube
     return (x[1]-x[0])*(y[1]-y[0])*(z[1]-z[0])
 
-def area_truncate(cube):
+def intersect(cube1, cube2):
+    cubes = set()
+    x1,y1,z1 = cube1 
+    x2,y2,z2 = cube2 
+    
+    if x1[0] > x2[1] or x1[1] < x2[0]:
+        return cubes
+    elif y1[0] > y2[1] or y1[1] < y2[0]:
+        return cubes
+    elif z1[0] > z2[1] or z1[1] < z2[0]:
+        return cubes
+    
+    x_vals = sorted({*x1, *x2})
+    y_vals = sorted({*x1, *x2})
+    z_vals = sorted({*x1, *x2})
+    x_min, y_min, z_min = None, None, None
+    
+    # cube 1
+    x1_min, x1_max = x1
+    y1_min, y1_max = y1
+    z1_min, z1_max = z1
+    for x_max in x_vals:
+        if x_min is None or x_min < x1_min:
+            x_min = x_max
+            continue
+        elif x_max > x_max: 
+            break
+        assert x_min < x_max
+        for y_max in y_vals:
+            if y_min is None or y_min < y1_min:
+                y_min = y_max
+                continue
+            elif y_max > y1_max:
+                break
+            assert y_min < y_max
+            for z_max in z_vals:
+                if z_min is None or z_min < z1_min:
+                    z_min = z_max
+                    continue
+                elif z_max > z1_max:
+                    break
+                assert z_min < z_max, f'{z_min} not less than {z_max}'
+                z_min = z_max
+            y_min = y_max
+            z_min = None
+        x_min = x_max
+        y_min = None
+
+    # cube 2
+    x2_min, x2_max = x2
+    y2_min, y2_max = y2
+    z2_min, z2_max = z2
+    for x_max in x_vals:
+        if x_min is None or x_min < x1_min:
+            x_min = x_max
+            continue
+        elif x_max > x_max: 
+            break
+        assert x_min < x_max
+        for y_max in y_vals:
+            if y_min is None or y_min < y1_min:
+                y_min = y_max
+                continue
+            elif y_max > y1_max:
+                break
+            assert y_min < y_max
+            for z_max in z_vals:
+                if z_min is None or z_min < z1_min:
+                    z_min = z_max
+                    continue
+                elif z_max > z1_max:
+                    break
+                cubes.add(
+                    ((x_min,x_max),(y_min,y_max),(z_min,z_max))
+                    )
+                assert z_min < z_max, f'{z_min} not less than {z_max}'
+                z_min = z_max
+            y_min = y_max
+            z_min = None
+        x_min = x_max
+        y_min = None
+    return cubes
+
+def subtract(cube1, cube2):
+    pass
+
+def truncate(cube):
     x,y,z = cube
     if x[0] > 50 or x[1] < -50:
-        return 0
+        return tuple()
     elif y[0] > 50 or y[1] < -50:
-        return 0
+        return tuple()
     elif z[0] > 50 or z[1] < -50:
-        return 0
+        return tuple()
     x_min, x_max = x
     x_min = max(x_min, -50)
     x_max = min(x_max, 50)
@@ -36,65 +122,25 @@ def area_truncate(cube):
     z_min, z_max = z 
     z_min = max(z_min, -50)
     z_max = min(z_max, 50)
-    return (x_max - x_min) * (y_max - y_min) * (z_max - z_min)
+    return (x_min, x_max), (y_min, y_max), (z_min, z_max)
     
 
 def puzzle1(test = True):
     temp = read_file(test)
-    x_values, y_values, z_values = set(), set(), set()
-    for state, coords in temp:
-        x,y,z = coords
-        x_values.update({*x})
-        y_values.update({*y})
-        z_values.update({*z})
-    x_values = {x for x in x_values if -50 <= x <= 50}
-    y_values = {x for x in y_values if -50 <= x <= 50}
-    z_values = {x for x in z_values if -50 <= x <= 50}
-    x_values = sorted(x_values)
-    y_values = sorted(y_values)
-    z_values = sorted(z_values)
-    disjoint_on = set()
-    for state, coords in temp:
-        x,y,z = coords
-        x_vals = sorted([i for i in x_values if x[0] <= i <= x[1]])
-        y_vals = sorted([i for i in y_values if y[0] <= i <= y[1]])
-        z_vals = sorted([i for i in z_values if z[0] <= i <= z[1]])
-        x_min, y_min, z_min = None, None, None
-        for x_max in x_vals:
-            if x_min is None:
-                x_min = x_max
-                continue
-            assert x_min < x_max
-            for y_max in y_vals:
-                if y_min is None:
-                    y_min = y_max
-                    continue
-                assert y_min < y_max
-                for z_max in z_vals:
-                    if z_min is None:
-                        z_min = z_max
-                        continue
-                    assert z_min < z_max, f'{z_min} not less than {z_max}'
-                    if state == 'off':
-                        try:
-                            disjoint_on.remove(
-                                ((x_min,x_max),(y_min,y_max),(z_min,z_max))
-                                )
-                        except:
-                            pass
-                    elif state == 'on':
-                        disjoint_on.add(
-                            ((x_min,x_max),(y_min,y_max),(z_min,z_max))
-                            )
-                    else:
-                        assert False
-                    z_min = z_max
-                y_min = y_max
-                z_min = None
-            x_min = x_max
-            y_min = None
+    for i, (state, coords) in enumerate(temp):
+        temp[i] = (state, truncate(coords))
+    disjoint = set()
+    for state, cube in temp:
+        new = set()
+        for coords in disjoint:
+            if state == 'on':
+                new.update(intersect(coords, cube))
+            elif state == 'off':
+                new.update(subtract(coords, cube))
+            else:
+                assert False
     total = 0
-    for cube in disjoint_on:
+    for cube in disjoint:
         total += area(cube)
     print(total)
                     
