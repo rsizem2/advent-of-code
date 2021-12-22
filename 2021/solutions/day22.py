75444cbab1a4fbb60576c9eaae8f541299f23c95
Robert Sizemore <50637844+rsizem2@users.noreply.github.com>
2021-12-22 12:31:24 -0500

    Update day22.py

--- 2021/solutions/day22.py
+++ 2021/solutions/day22.py
@@ -15,140 +15,56 @@ def read_file(test = True):
             temp.append((state, (x,y,z)))
     return temp
 
+def discretize(cube1, cube2):
+    assert overlap(cube1, cube2)
+    pass
+
 def area(cube):
     x,y,z = cube
     return (x[1]-x[0])*(y[1]-y[0])*(z[1]-z[0])
 
-def intersect(cube1, cube2):
-    cubes = set()
+def overlap(cube1, cube2):
     x1,y1,z1 = cube1 
     x2,y2,z2 = cube2 
-    
-    if x1[0] > x2[1] or x1[1] < x2[0]:
-        return cubes
+    if x1[0] > x2[1] or x1[1] < x2[0]: 
+        return False
     elif y1[0] > y2[1] or y1[1] < y2[0]:
-        return cubes
+        return False
     elif z1[0] > z2[1] or z1[1] < z2[0]:
-        return cubes
+        return False
+    return True
     
-    x_vals = sorted({*x1, *x2})
-    y_vals = sorted({*x1, *x2})
-    z_vals = sorted({*x1, *x2})
-    x_min, y_min, z_min = None, None, None
+def truncate(cube):
+    x,y,z = cube
     
-    # cube 1
-    x1_min, x1_max = x1
-    y1_min, y1_max = y1
-    z1_min, z1_max = z1
-    for x_max in x_vals:
-        if x_min is None or x_min < x1_min:
-            x_min = x_max
-            continue
-        elif x_max > x_max: 
-            break
-        assert x_min < x_max
-        for y_max in y_vals:
-            if y_min is None or y_min < y1_min:
-                y_min = y_max
-                continue
-            elif y_max > y1_max:
-                break
-            assert y_min < y_max
-            for z_max in z_vals:
-                if z_min is None or z_min < z1_min:
-                    z_min = z_max
-                    continue
-                elif z_max > z1_max:
-                    break
-                assert z_min < z_max, f'{z_min} not less than {z_max}'
-                z_min = z_max
-            y_min = y_max
-            z_min = None
-        x_min = x_max
-        y_min = None
-
-    # cube 2
-    x2_min, x2_max = x2
-    y2_min, y2_max = y2
-    z2_min, z2_max = z2
-    for x_max in x_vals:
-        if x_min is None or x_min < x1_min:
-            x_min = x_max
-            continue
-        elif x_max > x_max: 
-            break
-        assert x_min < x_max
-        for y_max in y_vals:
-            if y_min is None or y_min < y1_min:
-                y_min = y_max
-                continue
-            elif y_max > y1_max:
-                break
-            assert y_min < y_max
-            for z_max in z_vals:
-                if z_min is None or z_min < z1_min:
-                    z_min = z_max
-                    continue
-                elif z_max > z1_max:
-                    break
-                cubes.add(
-                    ((x_min,x_max),(y_min,y_max),(z_min,z_max))
-                    )
-                assert z_min < z_max, f'{z_min} not less than {z_max}'
-                z_min = z_max
-            y_min = y_max
-            z_min = None
-        x_min = x_max
-        y_min = None
-    return cubes
+    if not overlap(cube, ((-50,50),(-50,50),(-50,50))):
+        return None
+    
+    # truncate
+    x_min, x_max = max(x[0], -50), min(x[1], 50)
+    y_min, y_max = max(y[0], -50), min(y[1], 50)
+    z_min, z_max = max(z[0], -50), min(z[1], 50)
+    return (x_min, x_max), (y_min, y_max), (z_min, z_max)
 
 def subtract(cube1, cube2):
     pass
 
-def truncate(cube):
-    x,y,z = cube
-    if x[0] > 50 or x[1] < -50:
-        return tuple()
-    elif y[0] > 50 or y[1] < -50:
-        return tuple()
-    elif z[0] > 50 or z[1] < -50:
-        return tuple()
-    x_min, x_max = x
-    x_min = max(x_min, -50)
-    x_max = min(x_max, 50)
-    y_min, y_max = y
-    y_min = max(y_min, -50)
-    y_max = min(y_max, 50)
-    z_min, z_max = z 
-    z_min = max(z_min, -50)
-    z_max = min(z_max, 50)
-    return (x_min, x_max), (y_min, y_max), (z_min, z_max)
     
 
 def puzzle1(test = True):
     temp = read_file(test)
-    for i, (state, coords) in enumerate(temp):
-        temp[i] = (state, truncate(coords))
-    disjoint = set()
-    for state, cube in temp:
-        new = set()
-        for coords in disjoint:
-            if state == 'on':
-                new.update(intersect(coords, cube))
-            elif state == 'off':
-                new.update(subtract(coords, cube))
-            else:
-                assert False
     total = 0
-    for cube in disjoint:
-        total += area(cube)
+    for i, (state, coords) in enumerate(temp):
+        print(coords)
+        print(truncate(coords))
+        temp[i] = (state, truncate(coords))
+        total += area(coords)
     print(total)
                     
 
 def puzzle2(test = True):
     temp = read_file(test)
-
-                    
+        
 
 
 puzzle1()
